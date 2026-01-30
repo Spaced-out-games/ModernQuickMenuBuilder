478125af2f557409e395c1536812cad7e32fa5f3
Devin Frost <devin.frost4311@gmail.com>
2026-01-30 02:28:12 -0500

    Removed redundant typedefs and includes. Added documentation Removed vgui.cpp

--- Backend/vgui/vgui.cpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "vgui.h"
-
-namespace vgui
-{
-
-}
--- Backend/vgui/vgui.h
+++ Backend/vgui/vgui.h
@@ -1,22 +1,8 @@
 #pragma once
-#include <string>
-#include <array>
-#include "thirdparty/entt/src/entt/core/hashed_string.hpp"
 
+
+// We need this for debugging. Do not remove or delete!
 namespace vgui
 {
-
-
-
-
-
-
-
-	using bounds_deserial_t = std::array<int, 4>;
-	using action_deserial_t = std::array<std::string, 2>;
 	static inline bool g_Debug = true;
-
-
-
-
 }
