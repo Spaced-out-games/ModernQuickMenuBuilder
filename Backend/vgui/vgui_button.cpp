e2f9143fea56eb4b0f21f4fa81112a3e6b6467ad
Devin Frost <devin.frost4311@gmail.com>
2026-01-30 03:34:57 -0500

    Removed redundant function pointer nonsense Removed old on_event() code Simplified new on_event() handler

--- Backend/vgui/vgui_button.cpp
+++ Backend/vgui/vgui_button.cpp
@@ -10,45 +10,17 @@ namespace vgui
 
 
 
-    LRESULT VButton::on_event(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
-    {
-        int x = LOWORD(lp);
-        int y = HIWORD(lp);
 
-        if (msg == WM_LBUTTONDOWN && overlaps(x, y))
-        {
-            clicked = true;
-        }
-
-        if (msg == WM_LBUTTONUP)
-        {
-            if (overlaps(x, y) && clicked)
-            {
-                __debugbreak();
-                //m_Action.run(m_Owner, this);
-            }
-            clicked = false;
-        }
-
-        return 0;
-    }
 
     bool VButton::on_event(const Event& evt) {
 
         if (evt.type == EventType::LIFETIME_DTOR_EVENT) {
-            std::cout << "VButton got the dtor event";
+            std::cout << "VButton got the dtor event\n";
         }
 
         if (evt.type == EventType::MOUSE_CLICK_EVENT && overlaps(evt.mouse_event.x, evt.mouse_event.y))
         {
-            if (m_OnClick_pfn)
-            {
-                m_OnClick_pfn(evt.app, this, evt.mouse_event.x, evt.mouse_event.y);
-            }
-            else
-            {
-                throw std::runtime_error("m_OnClick_pfn was nullptr.");
-            }
+            m_Action.run(evt.app);
             return 1; // consumes the event
         }
         return false;
--- Backend/vgui/vgui_button.h
+++ Backend/vgui/vgui_button.h
@@ -11,22 +11,13 @@ namespace native
 
 namespace vgui
 {
+    // vgui button. Calls a callback. By default
     struct VButton : vgui::Widget
     {
+        VButton();
         Action      m_Action;
         bool        clicked = false;
-        
-        // By default, just do the action
-        void (*m_OnClick_pfn)(native::Application*, VButton*, int, int) = [](native::Application* app, VButton* btn, int x, int y) {
 
-            btn->m_Action.run(app);
-        };
-
-        // action binding
-        VButton();
-
-        // events
-        LRESULT on_event(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) override;
 
         bool on_event(const Event& evt) override;
 
