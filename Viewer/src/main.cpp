a03de85a605e5209f3ab6f9ca864ab6b35592dfb
Devin Frost <devin.frost4311@gmail.com>
2026-02-06 16:18:12 -0500

    Removed some redundant code Updated driver code to use less clunky and up-to-date methods for manipulating a LayerStack

--- Viewer/src/main.cpp
+++ Viewer/src/main.cpp
@@ -24,26 +24,26 @@ int main()
 
 
 
-
-
 #include "Backend/vgui/vgui_event_win32_impl.h"
 #include "Backend/vgui/vgui_window.h"
 #include "Backend/vgui/vgui_event.h"
 #include "Backend/qmb/QMBApplication.h"
 #include "Backend/vgui/vgui_background.h"
 #include "Backend/vgui/vgui_button.h"
-
+#include "Backend/woodchuck/woodchuck.h"
 #include <format>
 #include <memory>
 using namespace native;
 using namespace vgui;
 
-int main()
+int main(int argc, char** argv)
 {
-
-
-
-	
+	#ifdef _DEBUG
+		woodchuck::init<woodchuck::CLISink>();
+	#else
+		woodchuck::init<woodchuck::FileSink>("../QuickMenuBuilder.log");
+	#endif
+		woodchuck::setVerboseMode(false);
 
 
 	// Make an Windows application
@@ -53,47 +53,34 @@ int main()
 	app.init();
 
 	// make a Windows window, capable of VGUI rendering (this is what took forever)
-	vgui::VWindow win(&app);
+	vgui::VWindow window(&app);
 
-	// Initialize the window
-	win.init(app, (LPWSTR)L"QuickMenuBuilder v. 0.1", 100, 100, 500, 400);
+	app.link_window(window);
 
-
-	app.link_window(win);
-
-	std::unique_ptr<Layer> layer = std::make_unique<Layer>(0);
-	
-	std::unique_ptr<Widget> background = std::make_unique<VBackground>(win);
-	std::unique_ptr<Widget> button = std::make_unique<VButton>();
+	window.init(app, (LPWSTR)L"QuickMenuBuilder v. 0.7", 100, 100, 500, 400);
 
 
-	VBackground* bg = dynamic_cast<VBackground*>(background.get());
-	bg->img = qmb::Image::load_image("C:/Users/devin/Desktop/morty.jpg");
+	window.m_LayerStack.AddWidget<VBackground>("bg", Layer::BACK, window, "C:/Users/devin/Desktop/morty.jpg");
+	window.m_LayerStack.AddWidget<VButton>("button", 1);
 
-	layer->insert("bg", std::move(background));
-	layer->insert("button", std::move(button));
-	win.m_LayerStack.insert("layer", std::move(layer));
 
-	app.m_Window = &win;
-	win.m_Owner = static_cast<native::Application*>(&app);
+
 
 	MSG msg;
 
-	// main loop
+
+
 	while (!app.status) {
 		while (GetMessage(&msg, nullptr, 0, 0)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 
 
-			//vgui::vgui_event_win32_impl(&app, evt, msg);
 
-			//evt.free();
 
 			if (app.status) break;
 
 		}
 
 	}
-
 }
