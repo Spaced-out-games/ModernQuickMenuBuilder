e5b473d48a7d7d53c10aab6404b400450f6240b3
Devin Frost <devin.frost4311@gmail.com>
2026-02-06 16:12:21 -0500

    Added utility constants FRONT, BACK, INVALID_LAYER
    Fixed rendering logic/ made it more explicit

--- Backend/vgui/vgui_layer.h
+++ Backend/vgui/vgui_layer.h
@@ -2,21 +2,36 @@
 #include "../qmb/AcceleratedLinkedList.h"
 #include "vgui_widget.h"
 #include "vgui_event.h"
-
+#include <limits>
+#include <string>
+#undef min
+#undef max
 
 namespace vgui
 {
+
+
+
+
 	// Represents a set of sister widgets that should never overlap
 	struct Layer : qmb::AcceleratedLinkedList<std::string, Widget> {
-		int z = INT_MIN;
+		using depth_t = int;
+		static constexpr depth_t FRONT = std::numeric_limits<depth_t>::max();
+		static constexpr depth_t BACK = std::numeric_limits<depth_t>::min();
+		static constexpr depth_t INVALID_LAYER = std::numeric_limits<depth_t>::max() - 1;
+		depth_t z = BACK;
 		bool operator<(const Layer& rhs) const { return z < rhs.z; } // this might be the wrong sign
 		Layer() = default;
-		Layer(int z) : z(z) {}
+		Layer(depth_t z) : z(z) {}
 
 		bool on_event(const Event& evt) {
 
-			for (auto& widget : *this) {
-				if (widget.on_event(evt)) return 1;
+			//for (auto& widget : *this) {
+			//	if (widget.on_event(evt)) return 1;
+			//}
+
+			for (auto it = begin(); it != end(); ++it) {
+				if ((*it).on_event(evt)) return 1;
 			}
 
 			return 0;
@@ -24,9 +39,10 @@ namespace vgui
 
 
 		void draw(const Event& evt) {
-			for (auto& widget : *this) {
-				widget.draw((HDC)evt.window_paint_event.device);
+			for (auto it = rbegin(); it != rend(); ++it) {
+				it->draw((HDC)evt.window_paint_event.device);
 			}
+
 		}
 
 
